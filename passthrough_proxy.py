2544a44b511256bba421c2882a09b004a4a19f1d
vijayovirt <31509537+vijayovirt@users.noreply.github.com>
2017-09-01 10:15:00 +0530

    Update passthrough_proxy.py

--- passthrough_proxy.py
+++ passthrough_proxy.py
@@ -7,14 +7,27 @@ import requests
 app = Flask(__name__)
 
 #Hosted path is constant and will change based on machine where ovirt engine is hosted
-HOSTED_PATH = 'http://localhost:10000/'
+HOSTED_PATH = 'http://172.30.37.23/ovirt-engine/api'
 
-@app.route('/<url>')
+#@app.route('/<url>')
 def home(url):
     #
     url = HOSTED_PATH + url
     req = requests.get(url, stream = True)
     return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+
+
+@app.route('/<url>')
+def home(url):
+    #print url
+    requrl = "%s/%s/" %(HOSTED_PATH  ,url)
+    print requrl
+    session = requests.session()
+    session.auth = ('admin@internal', 'password')
+    request = getattr(session, 'get')
+    response  = request(requrl, verify=False)
+    return response
+
 
 if __name__ == '__main__':
     app.run()
